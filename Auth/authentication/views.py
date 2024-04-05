72bbdf8a0d7f081adca8b6aacea175933bff6752
elkdev72 <elijahkanyia.developer@gmail.com>
2024-04-05 23:10:09 +0300

    signupview

--- Auth/authentication/views.py
+++ Auth/authentication/views.py
@@ -8,4 +8,11 @@ def home(request):
 
 #signup page
 def user_signup(request):
-    
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
