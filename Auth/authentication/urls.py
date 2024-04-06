af6437bb9716b54924de973608b575e64cf0101b
elkdev72 <elijahkanyia.developer@gmail.com>
2024-04-06 20:55:13 +0300

    working

--- Auth/authentication/urls.py
+++ Auth/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path("",views.home,name="home"),
-    path("/login",views.user_login,name="login"),
-    path("/signup",views.user_signup,name="signup"),
-    path("/logout",views.user_logout,name='logout'),
+    path('login/', views.login_page, name='login_page'),    # Login page
+    path('register/', views.register_page, name='register'),  # Registration page
+    # path("/logout",views.user_logout,name='logout'),
 ]
